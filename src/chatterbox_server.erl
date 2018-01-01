0e375702a58cd48d779a0ca08bca54377f8429c9
Savas Aydin <savasaydin@gmail.com>
2018-01-01 21:49:37 +0100

    User can send a message
    A user that is connected to the server can send a message
    to another use that is also connected to the server.

--- src/chatterbox_server.erl
+++ src/chatterbox_server.erl
@@ -12,7 +12,8 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {listen_socket}).
+-record(state, {listen_socket,
+		users = []}).
 
 start(Port) ->
     gen_server:start({local, ?SERVER}, ?MODULE, [Port], []).
@@ -28,34 +29,40 @@ get_listening_socket() ->
 
 init([Port]) ->
     process_flag(trap_exit, true),
-    {ok, LSock} = gen_tcp:listen(Port, [binary, {active, false}]),
+    Opts = [binary, {active, false}, {reuseaddr, true}],
+    {ok, LSock} = gen_tcp:listen(Port, Opts),
     accept_incoming_connections(LSock),
     {ok, #state{listen_socket = LSock}}.
 
 handle_call(get_listening_socket, _, State) ->
     {reply, State#state.listen_socket, State};
+
+handle_call({subscribe, Username}, _, State) ->
+    Users = State#state.users,
+    NewState = State#state{users = [Username | Users]},
+    {reply, {ok, "subscribed"}, NewState};
+
+handle_call({unsubscribe, Username}, _, State) ->
+    Users = State#state.users,
+    NewState = State#state{users = Users -- [Username]},
+    {reply, {ok, "unsubscribed"}, NewState};
+
+handle_call(all_users, _, State) ->
+    Users = State#state.users,
+    Stringified = [atom_to_list(U) || U <- Users],
+    Reply = string:join(Stringified, ","),
+    {reply, Reply, State};
+
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+handle_cast(accepted, State) ->
+    accept_incoming_connections(State#state.listen_socket),
+    {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({tcp_closed, Socket}, State) ->
-    ok = gen_tcp:close(Socket),
-    {noreply, State};
-handle_info({tcp, Socket, <<"subscribe", Username/bitstring>>}, State) ->
-    Reply = message_handler:subscribe(binary_to_atom(Username, latin1)),
-    gen_tcp:send(Socket, Reply),
-    {noreply, State};
-handle_info({tcp, Socket, <<"list all users">>}, State) ->
-    Reply = message_handler:all_users(),
-    gen_tcp:send(Socket, Reply),
-    {noreply, State};
-handle_info({tcp, Socket, <<"unsubscribe", Username/bitstring>>}, State) ->
-    Reply = message_handler:unsubscribe(binary_to_atom(Username, latin1)),
-    gen_tcp:send(Socket, Reply),
-    {noreply, State};
 handle_info({'DOWN', Ref, process, _, _}, State) ->
     erlang:demonitor(Ref, [flush]),
     accept_incoming_connections(State#state.listen_socket),
@@ -63,17 +70,6 @@ handle_info({'DOWN', Ref, process, _, _}, State) ->
 handle_info(_, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 terminate(_Reason, State) ->
     LSock = State#state.listen_socket,
     case is_port(LSock) of
@@ -83,31 +79,59 @@ terminate(_Reason, State) ->
 	    ok
     end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 %%%===================================================================
-%%% Internal functions
-%%%===================================================================
 accept_incoming_connections(LSock) ->
-    APid = spawn(fun() -> acceptor(LSock) end),
-    erlang:monitor(process, APid).
+    Pid = spawn(fun() -> acceptor(LSock) end),
+    erlang:monitor(process, Pid).
 
 acceptor(LSock) ->
-    Chatterbox = whereis(?MODULE),
     case gen_tcp:accept(LSock) of
 	{ok, ASock} ->
-	    inet:setopts(ASock, [{active, true}]),
-	    gen_tcp:controlling_process(ASock, Chatterbox),
-	    acceptor(LSock);
+	    gen_server:cast(?MODULE, accepted),
+	    loop(ASock);
 	Error ->
 	    Error
     end.
+
+loop(Socket) ->
+    inet:setopts(Socket, [{active, once}]),
+    receive
+	{tcp_closed, Socket} ->
+	    ok = gen_tcp:close(Socket);
+	{tcp, Socket, <<"subscribe ", Username/bitstring>>} ->
+	    Reply = subscribe(binary_to_atom(Username, latin1)),
+	    ok = gen_tcp:send(Socket, Reply),
+	    loop(Socket);
+	{tcp, Socket, <<"list all users">>} ->
+	    Reply = all_users(),
+	    ok = gen_tcp:send(Socket, Reply),
+	    loop(Socket);
+	{tcp, Socket, <<"unsubscribe ", Username/bitstring>>} ->
+	    Reply = unsubscribe(binary_to_atom(Username, latin1)),
+	    ok = gen_tcp:send(Socket, Reply),
+	    loop(Socket);
+	{tcp, Socket, <<"send ", Data/bitstring>>} ->
+	    [Username | Msg] = string:tokens(binary_to_list(Data), " "),
+	    ok = gen_tcp:send(Socket, "sent"),
+	    list_to_atom(Username) ! {message_is_received, Msg},
+	    loop(Socket);
+	{message_is_received, Msg} ->
+	    gen_tcp:send(Socket, string:join(Msg, " ")),
+	    loop(Socket)
+	end.
+
+subscribe(Username) ->
+    {ok, Reply} = gen_server:call(?MODULE, {subscribe, Username}),
+    register(Username, self()),
+    Reply.
+
+all_users() ->
+    gen_server:call(?MODULE, all_users).
+
+unsubscribe(Username) ->
+    {ok, Reply} = gen_server:call(?MODULE, {unsubscribe, Username}),
+    unregister(Username),
+    Reply.
--- test/chatterbox_server_tests.erl
+++ test/chatterbox_server_tests.erl
@@ -52,48 +52,89 @@ chatterbox_server_user_commands_test_() ->
     {foreach,
      fun setup/0,
      fun cleanup/1,
-     [fun subscribe/0,
-      fun all_users/0,
-      fun unsubscribe/0
+     [
+      fun subscribe_with_a_username/0,
+      fun register_process_with_username_when_subscribing/0,
+      fun list_all_users/0,
+      fun unsubscribe_with_a_username/0,
+      fun unregister_process_when_unsubscribing/0,
+      fun send_message/0
      ]}.
 
-subscribe() ->
-    meck:expect(message_handler, subscribe, fun(_) -> "subscribed" end),
+subscribe_with_a_username() ->
     {ok, Sock} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
     Command = "subscribe Username",
     ok = gen_tcp:send(Sock, Command),
     Reply = receive_reply(Sock),
     ?assertEqual("subscribed", Reply).
 
-all_users() ->
-    meck:expect(message_handler, all_users, fun() -> "username" end),
+register_process_with_username_when_subscribing() ->
     {ok, Sock} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
+    Command = "subscribe Username",
+    ok = gen_tcp:send(Sock, Command),
+    "subscribed" = receive_reply(Sock),
+    ?assert(is_process_alive(whereis('Username'))).
+
+list_all_users() ->
+    {ok, Sock} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
+    Subscribe = "subscribe Username",
+    ok = gen_tcp:send(Sock, Subscribe),
+    "subscribed" = receive_reply(Sock),
     Command = "list all users",
     ok = gen_tcp:send(Sock, Command),
     Reply = receive_reply(Sock),
-    ?assertEqual("username", Reply).
+    ?assertEqual("Username", Reply).
 
-unsubscribe() ->
-    meck:expect(message_handler, unsubscribe, fun(_) -> "unsubscribed" end),
+unsubscribe_with_a_username() ->
     {ok, Sock} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
+    Subscribe = "subscribe Username",
+    ok = gen_tcp:send(Sock, Subscribe),
+    "subscribed" = receive_reply(Sock),
     Command = "unsubscribe Username",
     ok = gen_tcp:send(Sock, Command),
     Reply = receive_reply(Sock),
     ?assertEqual("unsubscribed", Reply).
+
+unregister_process_when_unsubscribing() ->
+    {ok, Sock} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
+    Subscribe = "subscribe Username",
+    ok = gen_tcp:send(Sock, Subscribe),
+    "subscribed" = receive_reply(Sock),
+    Command = "unsubscribe Username",
+    ok = gen_tcp:send(Sock, Command),
+    "unsubscribed" = receive_reply(Sock),
+    ?assertEqual(undefined, whereis('Username')).
+
+send_message() ->
+    {ok, Sock1} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
+    Subscribe1 = "subscribe Username1",
+    ok = gen_tcp:send(Sock1, Subscribe1),
+    "subscribed" = receive_reply(Sock1),
+
+    {ok, Sock2} = gen_tcp:connect({127, 0, 0, 1}, ?PORT, []),
+    Subscribe2 = "subscribe Username2",
+    ok = gen_tcp:send(Sock2, Subscribe2),
+    "subscribed" = receive_reply(Sock2),
+
+    Command = "send Username2 hello from Username1",
+    ok = gen_tcp:send(Sock1, Command),
+    ReplyFromSock1 = receive_reply(Sock1),
+    ?assertEqual("sent", ReplyFromSock1),
+
+    ReplyFromSock2 = receive_reply(Sock2),
+    ?assertEqual("hello from Username1", ReplyFromSock2).
 
 receive_reply(Sock) ->
     receive
 	{tcp, Sock, Reply} ->
 	    Reply
     after 100 ->
-	    {error, timeout}
+	    {error, client_timeout}
     end.
 
 setup() ->
     {ok, Pid} = chatterbox_server:start(?PORT),
-    meck:new(message_handler, [non_strict]),
     Pid.
 
 cleanup(Pid) ->
-    chatterbox_server:stop(Pid),
-    meck:unload(message_handler).
+    chatterbox_server:stop(Pid).
