a51782d99938be6720b29803ebecf089d1b3ed69
Savas Aydin <savasaydin@gmail.com>
2018-02-07 21:49:16 +0100

    Cleans up the logins table. Insert missing entries into the logins table.

--- src/chatterbox_debugger.erl
+++ src/chatterbox_debugger.erl
@@ -1,15 +1,19 @@
 -module(chatterbox_debugger).
 -export([create_table/0,
-         delete_table/0,
-         initialize/0,
-         increment_created_accounts/0,
-         increment_logged_accounts/0,
-         decrement_logged_accounts/0,
-         increment_created_rooms/0,
-         increment_failed_account_creation_attempts/0,
-         increment_failed_room_creation_attempts/0,
-         collect_logs/0
-        ]).
+	 delete_table/0,
+	 initialize/0,
+	 increment_created_accounts/0,
+	 increment_logged_accounts/0,
+	 decrement_logged_accounts/0,
+	 increment_created_rooms/0,
+	 increment_failed_account_creation_attempts/0,
+	 increment_failed_room_creation_attempts/0,
+	 collect_logs/0,
+	 insert_missing_logins/0,
+	 cleanup_dead_logins/0
+	]).
+
+-include("chatterbox.hrl").
 
 create_table() ->
     Res = chatterbox_lib:create_table_if_not_exist(?MODULE),
@@ -21,10 +25,10 @@ delete_table() ->
 
 initialize() ->
     Keys = [number_of_created_accounts,
-            number_of_created_rooms,
-            number_of_logged_in_accounts,
-            number_of_failed_account_creation_attempts,
-            number_of_failed_room_creation_attempts],
+	    number_of_created_rooms,
+	    number_of_logged_in_accounts,
+	    number_of_failed_account_creation_attempts,
+	    number_of_failed_room_creation_attempts],
     ets:insert(?MODULE, [{K, 0} || K <- Keys]).
 
 increment_created_accounts() ->
@@ -55,9 +59,9 @@ collect_logs() ->
     Logins = collect_login_data(),
     Counters = collect_debug_counters(),
     io:format("### Logged in users ###~n~s~n"
-              "### Counters ###~n~s~n",
-              [lists:flatten(Logins),
-               lists:flatten(Counters)]).
+	      "### Counters ###~n~s~n",
+	      [lists:flatten(Logins),
+	       lists:flatten(Counters)]).
 
 collect_login_data() ->
     LoginText = "~p logged in since ~p~n",
@@ -70,3 +74,20 @@ collect_debug_counters() ->
 collect(Table, Text) ->
     Entries = ets:tab2list(Table),
     [io_lib:format(Text, [Key, Value]) || {Key, Value} <- Entries].
+
+insert_missing_logins() ->
+    Accounts = ets:tab2list(accounts),
+    All = [?TO_ATOM(Name) || {Name, _} <- Accounts, is_alive(Name)],
+    Logins = [Name || {Name, _} <- ets:tab2list(logins)],
+    Missing = All -- Logins,
+    Entries = [{Name, calendar:local_time()} || Name <- Missing],
+    ets:insert(logins, Entries),
+    Missing.
+
+cleanup_dead_logins() ->
+    Dead = [Name || {Name, _} <- ets:tab2list(logins), not is_alive(Name)],
+    [ets:delete(logins, N) || N <- Dead],
+    Dead.
+
+is_alive(Name) ->
+    whereis(?TO_ATOM(Name)) /= undefined.
