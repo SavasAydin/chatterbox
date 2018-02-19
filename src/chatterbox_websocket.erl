3752b8847b3ed3807244e4ff4d136589a3a9d2b3
SPECTRE team <savas.aydin@ericsson.com>
2018-02-19 20:17:17 +0100

    Login using websocket

--- src/chatterbox_websocket.erl
+++ src/chatterbox_websocket.erl
@@ -6,14 +6,18 @@
 start(Port) ->
     Broadcaster = spawn_link(?MODULE, broadcast_server, [dict:new()]),
     Options = [{name, client_access},
-	       {loop, {?MODULE, loop, [Broadcaster]}},
-	       {port, Port}],
+               {loop, {?MODULE, loop, [Broadcaster]}},
+               {port, Port}],
     mochiweb_http:start_link(Options).
 
-ws_loop(Payload, Broadcaster, _ReplyChannel) ->
+ws_loop(Payload, Broadcaster, _) ->
     io:format("Received data: ~p~n", [Payload]),
-    Received = list_to_binary(Payload),
-    Broadcaster ! {broadcast, self(), Received},
+    Response = handle_request(Payload),
+    io:format("Received data:      ~p~n"
+              "Response generated  ~p~n",
+              [Payload, Response]),
+    Reply = build_response_message(Response),
+    Broadcaster ! {broadcast, self(), Reply},
     Broadcaster.
 
 loop(Req, Broadcaster) ->
@@ -28,22 +32,25 @@ loop(Req, _, false) ->
 loop(Req, Broadcaster, true) ->
     io:format(user, "loop true ~n", []),
     {ReentryWs, ReplyChannel} = mochiweb_websocket:upgrade_connection(
-				  Req, fun ?MODULE:ws_loop/3),
+                                  Req, fun ?MODULE:ws_loop/3),
     Broadcaster ! {register, self(), ReplyChannel},
     ReentryWs(Broadcaster).
 
 broadcast_server(Pids) ->
     Pids1 = receive
-		{register, Pid, Channel} ->
-		    broadcast_register(Pid, Channel, Pids);
-		{broadcast, Pid, Message} ->
-		    broadcast_sendall(Pid, Message, Pids);
-		{'DOWN', MRef, process, Pid, _Reason} ->
-		    broadcast_down(Pid, MRef, Pids);
-		Msg ->
-		    io:format("Unknown message: ~p~n", [Msg]),
-		    Pids
-	    end,
+                {register, Pid, Channel} ->
+                    io:format(user, "broadcast_server register~nPid    :~p~nChannel:~p~n", [Pid, Channel]),
+                    broadcast_register(Pid, Channel, Pids);
+                {broadcast, Pid, Message} ->
+                    io:format(user, "broadcast_server broadcast~nPid    :~p~nMessage:~p~n", [Pid, Message]),
+                    broadcast_sendall(Pid, Message, Pids);
+                {'DOWN', MRef, process, Pid, Reason} ->
+                    io:format(user, "broadcast_server down~nPid    :~p~nReason:~p~n", [Pid, Reason]),
+                    broadcast_down(Pid, MRef, Pids);
+                Msg ->
+                    io:format("Unknown message: ~p~n", [Msg]),
+                    Pids
+            end,
     erlang:hibernate(?MODULE, broadcast_server, [Pids1]).
 
 broadcast_register(Pid, Channel, Pids) ->
@@ -55,29 +62,30 @@ broadcast_down(Pid, MRef, Pids) ->
     NewPids = remove_pid(Pid, Pids, MRef),
     broadcast_sendall(Pid, "disconnected", NewPids).
 
-broadcast_sendall(Pid, Msg, Pids) ->
-    M = iolist_to_binary([pid_to_list(Pid), ": ", Msg]),
+broadcast_sendall(_, Msg, Pids) ->
+    %% M = iolist_to_binary([pid_to_list(Pid), ": ", Msg]),
     dict:fold(
       fun (K, {Reply, MRef}, Acc) ->
-	      try
-		  begin
-		      Reply(M),
-		      dict:store(K, {Reply, MRef}, Acc)
-		  end
-	      catch
-		  _:_ ->
-		      Acc
-	      end
+              try
+                  begin
+                      Reply(Msg),
+                      io:format(user, "K    :~p~nReply  :~p~nM      :~p~n", [K, Reply, Msg]),
+                      dict:store(K, {Reply, MRef}, Acc)
+                  end
+              catch
+                  _:_ ->
+                      Acc
+              end
       end,
       dict:new(),
       Pids).
 
 remove_pid(Pid, Pids, MRef) ->
     case dict:find(Pid, Pids) of
-	{ok, {_, MRef}} ->
-	    dict:erase(Pid, Pids);
-	_ ->
-	    Pids
+        {ok, {_, MRef}} ->
+            dict:erase(Pid, Pids);
+        _ ->
+            Pids
     end.
 
 doc_root() ->
@@ -87,3 +95,19 @@ local_path(Components) ->
     {file, Loaded} = code:is_loaded(?MODULE),
     ModPath = filename:dirname(filename:dirname(Loaded)),
     filename:join([ModPath | Components]).
+
+handle_request([Payload]) ->
+    [M, F | T] = string:tokens(binary_to_list(Payload), " "),
+    Module = list_to_atom(M),
+    Fun = list_to_atom(F),
+    Args = decode(T),
+    Module:Fun(Args).
+
+
+decode(Args) ->
+    Decoded = mochijson2:decode(Args, [{format, proplist}]),
+    [{binary_to_list(X), binary_to_list(Y)} || {X, Y} <- Decoded].
+
+build_response_message(Response) ->
+    Struct = {struct, [{response, list_to_binary(Response)}]},
+    mochijson2:encode(Struct).
